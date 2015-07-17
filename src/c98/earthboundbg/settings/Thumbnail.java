64f067e3d6480f42b86e1f9fdbd3042c689338a8
Caagr98 <Caagr98@users.noreply.github.com>
2015-07-17 18:46:06 +0200

    Switched the thumbnail loader to AsyncTask

--- src/c98/earthboundbg/settings/Thumbnail.java
+++ src/c98/earthboundbg/settings/Thumbnail.java
@@ -1,15 +1,13 @@
 package c98.earthboundbg.settings;
 
 import static c98.earthboundbg.settings.EBSettingsActivity.*;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import android.app.Activity;
 import android.graphics.Bitmap;
+import android.os.AsyncTask;
 import android.widget.ImageView;
 import c98.earthboundbg.Background;
 
 public class Thumbnail {
-	public static class ThumbTask implements Runnable {
+	public static class ThumbTask extends AsyncTask<Void, Void, Bitmap> {
 		private ImageView v;
 		private int id1, id2;
 		
@@ -19,41 +17,36 @@ public class Thumbnail {
 			id2 = j;
 		}
 		
-		@Override public void run() {
-			Activity a = (Activity)v.getContext();
+		@Override protected Bitmap doInBackground(Void... params) {
 			Bitmap b1 = getThumbnail(id1);
+			if(id2 == 0) return b1;
 			int[] rgb1 = new int[32 * 32];
 			b1.getPixels(rgb1, 0, 32, 0, 0, 32, 32);
-			if(id2 != 0) {
-				Bitmap b2 = getThumbnail(id2);
-				int[] rgb2 = new int[32 * 32];
-				b2.getPixels(rgb2, 0, 32, 0, 0, 32, 32);
-				
-				Bitmap b = Bitmap.createBitmap(32, 32, Bitmap.Config.ARGB_8888);
-				int[] rgb = new int[32 * 32];
-				for(int i = 0; i < rgb.length; i++) {
-					int rgb1_ = (rgb1[i] & 0xFEFEFE) >> 1;
-					int rgb2_ = (rgb2[i] & 0xFEFEFE) >> 1;
-					rgb[i] = rgb1_ + rgb2_ | 0xFF000000;
-				}
-				b.setPixels(rgb, 0, 32, 0, 0, 32, 32);
-				
-				thumbnails[id1 + id2 * COUNT] = b;
-				
-				b1 = b;
-			}
+			Bitmap b2 = getThumbnail(id2);
+			int[] rgb2 = new int[32 * 32];
+			b2.getPixels(rgb2, 0, 32, 0, 0, 32, 32);
 			
-			final Bitmap b = b1;
-			a.runOnUiThread(new Runnable() {
-				@Override public void run() {
-					if(v.getContentDescription().equals("" + id1 + "," + id2)) v.setImageBitmap(b);
-				}
-			});
+			Bitmap b = Bitmap.createBitmap(32, 32, Bitmap.Config.ARGB_8888);
+			int[] rgb = new int[32 * 32];
+			for(int i = 0; i < rgb.length; i++) {
+				int rgb1_ = (rgb1[i] & 0xFEFEFE) >> 1;
+				int rgb2_ = (rgb2[i] & 0xFEFEFE) >> 1;
+				rgb[i] = rgb1_ + rgb2_ | 0xFF000000;
+			}
+			b.setPixels(rgb, 0, 32, 0, 0, 32, 32);
+			
+			thumbnails[id1 + id2 * COUNT] = b;
+			
+			return b;
 		}
+		
+		@Override protected void onPostExecute(Bitmap result) {
+			if(v.getContentDescription().equals("" + id1 + "," + id2)) v.setImageBitmap(result);
+		}
+		
 	}
 	
 	private static volatile Bitmap[] thumbnails = new Bitmap[COUNT * COUNT];
-	private static ExecutorService exec = Executors.newSingleThreadExecutor();
 	
 	public static void getThumbnail(ImageView view, int i, int j) {
 		if(thumbnails[i + j * COUNT] != null) {
@@ -62,7 +55,7 @@ public class Thumbnail {
 		}
 		view.setContentDescription("" + i + "," + j);
 		view.setImageDrawable(null);
-		exec.execute(new ThumbTask(view, i, j));
+		new ThumbTask(view, i, j).execute();
 	}
 	
 	private static Bitmap getThumbnail(int num) {
